518b337ce73ae521dfc3de8c7289fab4652a1bf7
Dabi-Jay <agosujubril@gmail.com>
2026-07-07 14:11:21 +0100

    Creating a new landing page, and adding create events functionality

--- community/urls.py
+++ community/urls.py
@@ -5,6 +5,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home_view, name= "home"),
+    path('events/', views.events_view, name='events_page'),
+    path('events/create/', views.create_event_view, name='create_event'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('register/', views.register_view, name='register'),
--- community/views.py
+++ community/views.py
@@ -2,28 +2,46 @@ from django.shortcuts import render, redirect
 from .models import Event
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .forms import EventForm
 
-# Create your views here.
 
 def home_view(request):
-    events = Event.objects.all()
+    return render(request, 'home/home.html') 
 
+
+def events_view(request):
+    events = Event.objects.all()
+    
     context = {
         "events": events,
         "event_count": events.count()
     }
-    return render(request, 'home/home.html', context)
+    return render(request, 'events/events_page.html', context) 
 
 
-def register_view (request):
+@login_required
+def create_event_view(request):
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.created_by = request.user # Automatically logs who made it
+            event.save()
+            return redirect("events_page") # Bounces right back to the feed to view it!
+    else:
+        form = EventForm()
+        
+    return render(request, "events/create_event.html", {"form": form})
+
+def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("home")
+            return redirect("events_page") 
     else:
         form = UserCreationForm()
 
     return render(request, "registration/register.html", {"form": form})
-
