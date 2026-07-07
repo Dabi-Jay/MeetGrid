e0e0c214257b943629c054d8491fc6c5337f9812
Dabi-Jay <agosujubril@gmail.com>
2026-07-07 07:59:52 +0100

    Implementing user login using Django built in login_view

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path("", views.home_view, name= "home"),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
