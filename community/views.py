8c70a79d5b210262cd8d1c8ed8789973a4ca956a
Dabi-Jay <agosujubril@gmail.com>
2026-07-07 09:53:51 +0100

    Registration and logging out

--- community/views.py
+++ community/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Event
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -11,4 +13,17 @@ def home_view(request):
         "event_count": events.count()
     }
     return render(request, 'home/home.html', context)
-    
+
+
+def register_view (request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/register.html", {"form": form})
+
